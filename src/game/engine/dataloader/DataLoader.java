63774f3a8b0aa5a957dbbc10129005ebc33f3c51
Carlos Habib <carlosemad156@gmail.com>
2026-03-14 11:17:15 +0200

    omar is stupid

---
+++ src/game/engine/dataloader/DataLoader.java
@@ -0,0 +1,30 @@
+package game.engine.dataloader;
+import game.engine.cards.Card;
+import game.engine.cells.Cell;
+import game.engine.monsters.Monster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class DataLoader {
+    private String
+            CARDS_FILE_NAME = "cards.csv",
+            CELLS_FILE_NAME = "cells.csv",
+            MONSTERS_FILE_NAME = "monsters.csv";
+
+    public static ArrayList<Card> readCards() throws IOException {
+        ArrayList<Card> cards = new ArrayList<>();
+
+    }
+
+    public static ArrayList<Cell> readCells() throws IOException{
+        ArrayList<Cell> cards = new ArrayList<>();
+    }
+
+    public static ArrayList<Monster> readMonsters() throws IOException{
+        ArrayList<Monster> cards = new ArrayList<>();
+    }
+
+}
