3b1cee19b6f6a3bf0796c01892ec2c314254d060
Omar <omarradi2008@gmail.com>
2026-03-14 10:40:21 +0200

    test

--- src/game/engine/Constants.java
+++ src/game/engine/Constants.java
@@ -4,20 +4,20 @@ import java.security.Key;
 import java.security.PrivateKey;
 
 public final class Constants {
-    private int BOARD_SIZE = 100;
-    private int BOARD_ROWS = 10;
-    private int BOARD_COLS = 10;
-    private int WINNING_POSITION = 99;
-    private int STARTING_POSITION = 0;
-    private int[] MONTER_CELL_INDICES = {2, 18, 34, 54, 82, 88};
-    private int[] CONVEYOR_CELL_INDICES = {6, 22, 44, 52, 66};
-    private int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98};
-    private int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
-    int WINNING_ENERGY = 1000;
-    int MIN_ENERGY = 0;
-    int MULTITASKER_BONUS = 200;
-    int SCHEMER_STEAL = 10;
-    int SLIP_PENALTY = 100;
-    int POWERUP_COST = 500;
+    public int BOARD_SIZE = 100;
+    public int BOARD_ROWS = 10;
+    public int BOARD_COLS = 10;
+    public int WINNING_POSITION = 99;
+    public int STARTING_POSITION = 0;
+    public int[] MONTER_CELL_INDICES = {2, 18, 34, 54, 82, 88};
+    public int[] CONVEYOR_CELL_INDICES = {6, 22, 44, 52, 66};
+    public int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98};
+    public int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
+    public int WINNING_ENERGY = 1000;
+    public int MIN_ENERGY = 0;
+    public int MULTITASKER_BONUS = 200;
+    public int SCHEMER_STEAL = 10;
+    public int SLIP_PENALTY = 100;
+    public int POWERUP_COST = 500;
 
 }
