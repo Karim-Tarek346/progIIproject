36120464b90df5b7ccbf5c7cfd527c39a665f692
Omar <omarradi2008@gmail.com>
2026-03-14 09:18:59 +0200

    test

---
+++ src/game/engine/cells/Cell.java
@@ -0,0 +1,27 @@
+package game.engine.cells;
+
+import game.engine.monsters.Monster;
+
+public class Cell {
+    private String name; //Get only
+    private Monster monster;
+
+    public String getName() {
+        return name;
+    }
+
+    public Monster getMonster() {
+        return monster;
+    }
+
+    public void setMonster(Monster monster) {
+        this.monster = monster;
+    }
+
+    public Cell(String name){
+        this.name = name;
+        this.monster = null;
+    }
+
+
+}
