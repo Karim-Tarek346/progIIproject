091cd9a776c7e482503dd5d1a69acb8b5eed7b13
lenovo <omarradi2008@gmail.com>
2026-03-25 17:34:55 +0200

    Final Step

--- src/game/engine/Board.java
+++ src/game/engine/Board.java
@@ -1,5 +1,50 @@
 package game.engine;
+import game.engine.cells.Cell;
+import game.engine.monsters.Monster;
+import game.engine.cards.Card;
+import game.engine.Constants;
+
+import java.util.ArrayList;
+
+import static game.engine.dataloader.DataLoader.readCards;
 
 public class Board {
-    
+    private Cell[][] boardCells; // Getter only
+    private static ArrayList<Monster> stationedMonsters; //Getter and Setter
+    private static ArrayList<Card> originalCards; // Getter only
+    private static ArrayList<Card> cards; //Getter and Setter
+
+    public static ArrayList<Monster> getStationedMonsters() {
+        return stationedMonsters;
+    }
+
+    public static ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public Cell[][] getBoardCells() {
+        return boardCells;
+    }
+
+    public static ArrayList<Card> getOriginalCards() {
+        return originalCards;
+    }
+
+    public static void setCards(ArrayList<Card> cards) {
+        Board.cards = cards;
+    }
+
+    public static void setStationedMonsters(ArrayList<Monster> stationedMonsters) {
+        Board.stationedMonsters = stationedMonsters;
+    }
+
+    public Board(ArrayList<Card> readCards){
+        stationedMonsters = new ArrayList<>();
+        cards = new ArrayList<>();
+        originalCards = readCards;
+        this.boardCells = new Cell[Constants.BOARD_ROWS][Constants.BOARD_COLS];
+
+    }
+
+
 }
--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -1,5 +1,46 @@
 package game.engine;
+import game.engine.monsters.Monster;
+import game.engine.dataloader.DataLoader;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
-    
+    private Board board; //Getter only
+    private ArrayList<Monster> allMonsters ; //Getter only
+    private Monster player; //Getter
+    private Monster opponent; //Getter
+    private Monster current; // Getter and setter
+
+
+    private Monster selectRandomMonstersByRole(Role role){
+
+        ArrayList<Monster> filtered = new ArrayList<>();
+
+        for(Monster m : allMonsters){
+            if(m.getRole() == role){
+                filtered.add(m);
+            }
+        }
+
+        return filtered.get((int)Math.random() * filtered.size());
+    }
+
+
+    public Game(Role playerRole) throws IOException {
+        this.board = new Board(DataLoader.readCards());
+        this.allMonsters = DataLoader.readMonsters();
+        this.player = selectRandomMonstersByRole(playerRole);
+
+        if(playerRole == Role.SCARER)
+            this.opponent = selectRandomMonstersByRole(Role.LAUGHER);
+        else
+            this.opponent = selectRandomMonstersByRole(Role.SCARER);
+
+        this.current = player;
+
+    }
+
 }
