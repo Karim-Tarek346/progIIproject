74cfacd9b1f7b95cb3e7f21b0f402f9a0ed35e5b
lenovo <omarradi2008@gmail.com>
2026-04-29 22:48:19 +0300

    Conveyorbelt onLand implementation DoorCell onLand implementation

--- src/game/engine/cells/ConveyorBelt.java
+++ src/game/engine/cells/ConveyorBelt.java
@@ -1,5 +1,8 @@
 package game.engine.cells;
 
+import game.engine.exceptions.InvalidMoveException;
+import game.engine.monsters.Monster;
+
 public class ConveyorBelt extends TransportCell {
 
     public ConveyorBelt(String name, int effect)
@@ -7,5 +10,13 @@ public class ConveyorBelt extends TransportCell {
         super(name,effect);
     }
 
+    @Override
+    public void onLand(Monster landingMonster, Monster opponentMonster) throws InvalidMoveException {
+        // 1. Always call super to register the landing monster on the cell
+        super.onLand(landingMonster, opponentMonster);
+
+        // 2. Trigger the transport effect on the monster that just landed
+        this.transport(landingMonster);
+    }
 
 }
--- src/game/engine/cells/DoorCell.java
+++ src/game/engine/cells/DoorCell.java
@@ -1,7 +1,12 @@
 package game.engine.cells;
 
+import game.engine.Board;
 import game.engine.Role;
+import game.engine.exceptions.InvalidMoveException;
 import game.engine.interfaces.CanisterModifier;
+import game.engine.monsters.Monster;
+
+import java.util.ArrayList;
 
 public class DoorCell extends Cell implements CanisterModifier {
     private Role role; //Getter only
@@ -13,6 +18,55 @@ public class DoorCell extends Cell implements CanisterModifier {
         this.role = role;
         this.energy = energy;
         this.activated = false;
+    }
+
+    @Override
+    public void onLand(Monster landingMonster, Monster opponentMonster) throws InvalidMoveException {
+        // 1. Execute base cell logic to register the landing monster
+        super.onLand(landingMonster, opponentMonster); // [cite: 73]
+
+        // 2. Check if the door is not yet activated
+        if (!this.isActivated()) {
+            boolean energyChanged = false;
+
+            // 3. Determine the energy modifier based on whose side the door is on
+            int energyModifier;
+            if (landingMonster.getRole() == this.getRole()) {
+                energyModifier = this.getEnergy(); // Positive gain if roles match [cite: 81]
+            } else {
+                energyModifier = -this.getEnergy(); // Penalty if roles mismatch [cite: 81]
+            }
+
+            // 4. Modify the landing monster's energy
+            int initialLandingEnergy = landingMonster.getEnergy();
+
+            // Use alterEnergy to automatically respect the shield mechanic [cite: 43, 81]
+            // Note: If you implemented CanisterModifier, you might call modifyCanisterEnergy() here instead
+            landingMonster.alterEnergy(energyModifier);
+
+            if (landingMonster.getEnergy() != initialLandingEnergy) {
+                energyChanged = true;
+            }
+
+            // 5. Modify stationed monsters of the same role as the landing monster
+            // Note: Adjust Board.getStationedMonsters() based on how you access your stationed monsters array/list
+            for (Monster stationedMonster : Board.getStationedMonsters()) { // [cite: 81]
+                if (stationedMonster.getRole() == landingMonster.getRole()) { // [cite: 81]
+                    int initialStationedEnergy = stationedMonster.getEnergy();
+
+                    stationedMonster.alterEnergy(energyModifier); // Respects shield [cite: 81]
+
+                    if (stationedMonster.getEnergy() != initialStationedEnergy) {
+                        energyChanged = true;
+                    }
+                }
+            }
+
+            // 6. Mark as activated ONLY if energy was gained or lost (e.g., not fully blocked by a shield)
+            if (energyChanged) { // [cite: 81]
+                this.setActivated(true); // [cite: 81]
+            }
+        }
     }
 
     public int getEnergy() {
